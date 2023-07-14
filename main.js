4a225b8008df327e1bd29e4515ce845508e26bac
programming hero <naofelbadsha@gmail.com>
2023-07-14 09:13:01 +0100

    adderd the home

--- main.js
+++ main.js
@@ -24,3 +24,23 @@ document.querySelector('#CloseLoginForm').onclick = () => {
     document.querySelector('.loginFormomContainer').classList.remove('active')
 };
 //------------Login------------Button---------------//
+
+
+
+//---------Home--------Parallex--------Effect-----------//
+document.querySelector(".Home").onmousemove = (e) =>{
+    document.querySelectorAll(".homeParallexEffect").forEach((el) => {
+      let Speed = el.getAttribute("data-speed")
+      let X = (window.innerWidth - e.pageX * Speed) / 50;
+      let Y = (window.innerHeight - e.pageY * Speed) / 50;
+  
+      el.style.transform = `translateX(${Y}px) translateY(${X}px)`;
+    }); 
+    };
+  
+    document.querySelector(".Home").onmouseleave = (e) =>{
+      document.querySelectorAll(".homeParallexEffect").forEach((el) => {
+        el.style.transform = `translateX(0px) translateY(0px)`;
+      }); 
+      };
+  //---------Home--------Parallex--------Effect-----------//
