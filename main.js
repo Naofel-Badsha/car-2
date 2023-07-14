4bc56f7e8f69c0632f614b89d33be6f6a6e9788e
programming hero <naofelbadsha@gmail.com>
2023-07-14 09:18:59 +0100

    added icons

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
-  menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 };
 
-  window.onscroll = () =>{
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 };
@@ -28,19 +28,39 @@ document.querySelector('#CloseLoginForm').onclick = () => {
 
 
 //---------Home--------Parallex--------Effect-----------//
-document.querySelector(".Home").onmousemove = (e) =>{
+document.querySelector(".Home").onmousemove = (e) => {
     document.querySelectorAll(".homeParallexEffect").forEach((el) => {
-      let Speed = el.getAttribute("data-speed")
-      let X = (window.innerWidth - e.pageX * Speed) / 50;
-      let Y = (window.innerHeight - e.pageY * Speed) / 50;
-  
-      el.style.transform = `translateX(${Y}px) translateY(${X}px)`;
-    }); 
-    };
-  
-    document.querySelector(".Home").onmouseleave = (e) =>{
-      document.querySelectorAll(".homeParallexEffect").forEach((el) => {
+        let Speed = el.getAttribute("data-speed")
+        let X = (window.innerWidth - e.pageX * Speed) / 50;
+        let Y = (window.innerHeight - e.pageY * Speed) / 50;
+
+        el.style.transform = `translateX(${Y}px) translateY(${X}px)`;
+    });
+};
+
+document.querySelector(".Home").onmouseleave = (e) => {
+    document.querySelectorAll(".homeParallexEffect").forEach((el) => {
         el.style.transform = `translateX(0px) translateY(0px)`;
-      }); 
-      };
-  //---------Home--------Parallex--------Effect-----------//
+    });
+};
+//---------Home--------Parallex--------Effect-----------//
+
+
+
+//---------Icons------------Container---------Start---------//
+let valueDidplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valueDidplays.forEach((valueDidplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDidplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        valueDidplay.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+});
+//---------Icons------------Container---------End---------//
